b2e99f2d49842be01e2ae1f1f627c0d53d12a178
unknown <nuhlamasri@gmail.com>
2024-10-10 01:20:46 +0300

    Create Helper file

--- src/helper.py
+++ src/helper.py
@@ -13,6 +13,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 """" ------------------------------------------------------------
 -----------------------------------------------------------------
+- function name : load_pdf
 - function description :Load PDF files 
 - arguments data:  is the directory where we saved the pfd files 
 - returned value : all documents in this folder  
@@ -30,9 +31,31 @@ def load_pdf(data):
 
 """" ------------------------------------------------------------
 -----------------------------------------------------------------
-- function description :Load PDF files 
-- arguments data:  is the directory where we saved the pfd files 
-- returned value : all documents in this folder  
+- function name : text_splinting
+- function description :splits text iin the document wee got into 
+  chunks so we can vectorized it to dataset 
+- arguments data:  documents we loaded from folder 
+- returned value : chunks of text 
 ----------------------------------------------------------------
 ----------------------------------------------------------------
 """
+def text_splinting(extracted_data):
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+    text_chunks=text_splitter.split_documents(extracted_data)
+    return text_chunks 
+
+
+"""" ------------------------------------------------------------
+-----------------------------------------------------------------
+- function name : download_hugging_face_embeddings
+- function description :download embedding Model we uses to 
+  vectoring our data
+- arguments data:  The Model name from HuggingFace - ini value 
+  will be {sentence-transformers/all-MiniLM-L6-v2}
+- returned value : embeddings Model
+----------------------------------------------------------------
+----------------------------------------------------------------
+"""
+def download_hugging_face_embeddings(model_name="sentence-transformers/all-MiniLM-L6-v2"):
+    embeddings=HuggingFaceEmbeddings(model_name=model_name)
+    return embeddings
